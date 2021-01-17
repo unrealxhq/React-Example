95ed2417c4ef47cee1e175260bc512a9264d204a
unrealxhq <jordandarby53@gmail.com>
2021-01-17 09:18:55 +0000

    Add files via upload

---
+++ react_PLD.lua
@@ -0,0 +1,137 @@
+return {
+    ["Lady Lilith"]={
+        ["Dark Thorn"]={
+            ["complete_reaction"]="input //gs c dispel", 
+            ["ready_reaction"]="turnaround"
+        },
+    }, 
+	["Kei"]={
+        ["Dancing Fullers"]={
+            ["complete_reaction"]="runto 3", 
+            ["ready_reaction"]="runaway 12"
+        }
+    },	    
+	["Lady Lilith"]={
+        ["Fatal Allure"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        }
+    },
+    ["Bloodcrown Brradhod"]={
+        ["Arm block"]={
+            ["complete_reaction"]="input /ma 'Dispel' <bt>", 
+            ["ready_reaction"]=""
+        }
+    },
+	["Za'Dha Adamantking"]={
+        ["Howl"]={
+            ["complete_reaction"]="input /ma 'Dispel' <bt>", 
+            ["ready_reaction"]=""
+        },
+		["Shell Guard"]={
+            ["complete_reaction"]="input /ma 'Dispel' <bt>", 
+            ["ready_reaction"]=""
+        }
+    },
+	["Bozzetto Freyja"]={
+        ["Tribulation"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+		},
+        ["Edifying Dance"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+		},
+		["Petrifying Dance"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="turnaround"
+			},
+		["Petrifaction"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="turnaround"
+			},
+		["Mind Blast"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="turnaround"
+			},
+		["Imperiling Disregard"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="facemob"
+			},
+		["Implaing Disregard"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="facemob"
+			},
+		["Impaling Disregard"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="facemob"
+			},
+    }, 
+    ["Bozzetto Frigg"]={
+        ["Bugle Call"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        },
+		["Raqs Baladi Dance"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="turnaround"
+			},
+		["Petrifying Dance"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="turnaround"
+			},
+		["Chastening Disregard"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="facemob"
+			},
+	}, 
+    ["Bozzetto Skathi"]={
+        ["Deafening Tantara"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        }, 
+        ["Tribulation"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        },
+        ["Reprobation"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        },
+        ["Noxious Dance"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        },
+        ["Mind Blast"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        },
+        ["Blight Dance"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        },
+		["Imperiling Disregard"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="facemob"
+			},
+		["Implaing Disregard"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="facemob"
+			},
+		["Impaling Disregard"]={
+				["complete_reaction"]="facemob", 
+				["ready_reaction"]="facemob"
+			},
+        ["Petrifaction"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        }
+    },
+	["Bozzetto Reaver"]={
+        ["Dread Spikes"]={
+            ["complete_reaction"]="gs c dispel_reaver", 
+            ["ready_reaction"]=""
+        }
+    },
+
+}
